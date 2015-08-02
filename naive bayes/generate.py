93742e803f7c433f0137a15a0670f05191573832
duaraghav8@gmail <duaraghav8@gmail.com>
2015-08-02 19:28:29 +0530

    Bug Fix

--- naive bayes/generate.py
+++ naive bayes/generate.py
@@ -3,7 +3,7 @@ from random import *;
 
 if (__name__ == '__main__'):
 	concept = {
-		'a1' : '1,7,4,10,11,19,22,25,16,15,12',
+		'a1' : ',1,7,4,10,11,19,22,25,16,15,12,',
 		'a2' : '?',
 		'a3' : '1,3,5,7,9,11,13,15,17,19,21,24,22'
 	};
@@ -16,7 +16,7 @@ if (__name__ == '__main__'):
 			attrList.append (attributes);
 			terms = attributes.split ();
 
-			if (not (concept ['a1'].find (terms [0]) == -1) and not (concept ['a3'].find (terms [2]) == -1)):
+			if (not (concept ['a1'].find (',' + terms [0] + ',') == -1) and not (concept ['a3'].find (',' + terms [2] + ',') == -1)):
 				attrClass.append ('+');
 			else:
 				attrClass.append ('-');
